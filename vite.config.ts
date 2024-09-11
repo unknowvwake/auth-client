fb05e00aee536a42dac6384a3e73228d1e240ecd
thisyahlen <104053934+thisyahlen-deriv@users.noreply.github.com>
2024-09-11 10:31:26 +0800

    fix: add tsconfig plugin to vite

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,11 @@ import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), dts({ include: ['src'], exclude: ['src/**/*.spec.tsx'] })],
+    plugins: [react(), dts({
+        include: ['src'],
+        exclude: ['src/**/*.spec.tsx'],
+        tsconfigPath: './tsconfig.app.json'
+    })],
     build: {
         lib: {
             entry: resolve(__dirname, 'src/index.ts'),
