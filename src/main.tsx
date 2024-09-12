6fad510833feaff2926d83579dfb39490727d882
Thisyahlen <thisyahlen@regentmarkets.com>
2024-09-12 09:26:03 +0800

    chore: add logout ws call in context

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
 import './index.css';
+import { AppDataProvider } from '@deriv-com/api-hooks';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
+        <QueryClientProvider client={new QueryClient()}>
+            <AppDataProvider>
+                <App />
+            </AppDataProvider>
+        </QueryClientProvider>
     </StrictMode>
 );
