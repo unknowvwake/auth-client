ea8404c9bc072060c68b7de5295628c1b35f2228
adrienne-rio <adrienne@deriv.com>
2024-09-26 17:21:01 +0800

    chore: removed console logs and await logout function

--- src/hooks/useOAuth2.ts
+++ src/hooks/useOAuth2.ts
@@ -32,11 +32,9 @@ export const useOAuth2 = (OAuth2GrowthBookConfig: OAuth2GBConfig, WSLogoutAndRed
 
         const onMessage = async (event: MessageEvent) => {
             const allowedOrigin = getOAuthOrigin();
-            console.log(event)
-            console.log(allowedOrigin)
             if (allowedOrigin === event.origin) {
                 if (event.data === 'logout_complete') {
-                    WSLogoutAndRedirect();
+                    await WSLogoutAndRedirect();
                 } else {
                     console.warn('Unexpected message received: ', event.data);
                 }
@@ -51,7 +49,7 @@ export const useOAuth2 = (OAuth2GrowthBookConfig: OAuth2GBConfig, WSLogoutAndRed
 
     const OAuth2Logout = useCallback(async () => {
         if (!isOAuth2Enabled) {
-            WSLogoutAndRedirect();
+            await WSLogoutAndRedirect();
             return;
         }
 
@@ -62,8 +60,8 @@ export const useOAuth2 = (OAuth2GrowthBookConfig: OAuth2GBConfig, WSLogoutAndRed
             iframe.style.display = 'none';
             document.body.appendChild(iframe);
 
-            setTimeout(() => {
-                WSLogoutAndRedirect();
+            setTimeout(async () => {
+                await WSLogoutAndRedirect();
             }, 10000);
         }
 
